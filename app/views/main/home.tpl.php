66d684feca80975794911ce03504ee9f28584d4c
celin <celin@localhost>
2021-08-05 10:21:14 +0200

    files database and config.ini

--- app/views/main/home.tpl.php
+++ app/views/main/home.tpl.php
@@ -1,1 +1,9 @@
-<button class="button_add_project">Nouveau Projet</button>
+<main class="main">
+    <button class="button_add_project">Nouveau Projet</button>
+    <div class="main_section">
+        <h2 class="main_title">Mes clients acheteurs</h2>
+    </div>
+    <div class="main_section">
+        <h2 class="main_title">Mes clients vendeurs</h2>
+    </div>
+</main>
