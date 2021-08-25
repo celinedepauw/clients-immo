9a2093ba2346455d48281f54b5d2214f2014d4c7
celin <celin@localhost>
2021-08-25 19:25:51 +0200

    link to a project by clicking on a small project

--- app/views/project/purchases.tpl.php
+++ app/views/project/purchases.tpl.php
@@ -1,15 +1,14 @@
 <h2 class="purchases_title">Mes Clients Acheteurs</h2>
-
-<?php foreach ($purchasesList as $currentPurchase) : ?>
     <div class="all_purchases">
-        <div class="purchaseSmall">
-            <h3 class="purchaseSmall_client"><?= strtoupper($currentPurchase->getClientLastname()) . " " . ucfirst($currentPurchase->getClientFirstname()) ?></h3>
-            <div class="purchaseSmall_type"><?= ucfirst($currentPurchase->getTypeName()) ?></div>
-            <div class="purchaseSmall_rooms"><?= $currentPurchase->getProjectRooms() ?> chambres</div>
-            <div class="purchaseSmall_location"><?= ucfirst($currentPurchase->getProjectLocation()) ?></div>
-            <div class="purchaseSmall_price">Budget : <?= $currentPurchase->getProjectPrice() . " €" ?></div>
-            <a href="<?= $router->generate('purchase', ['idProject' => $currentPurchase->getId()])?>">Lien détails</a>
-        </div>
-        
-    </div>
+    <?php foreach ($purchasesList as $currentPurchase) : ?>
+        <a href="<?= $router->generate('purchase', ['idProject' => $currentPurchase->getId()])?>">
+            <div class="purchaseSmall">
+                <h3 class="purchaseSmall_client"><?= strtoupper($currentPurchase->getClientLastname()) . " " . ucfirst($currentPurchase->getClientFirstname()) ?></h3>
+                <div class="purchaseSmall_type"><?= ucfirst($currentPurchase->getTypeName()) ?></div>
+                <div class="purchaseSmall_rooms"><?= $currentPurchase->getProjectRooms() ?> chambres</div>
+                <div class="purchaseSmall_location"><?= ucfirst($currentPurchase->getProjectLocation()) ?></div>
+                <div class="purchaseSmall_price">Budget : <?= $currentPurchase->getProjectPrice() . " €" ?></div>
+            </div>    
+        </a>
     <?php endforeach ?>
+    </div>
--- app/views/project/sales.tpl.php
+++ app/views/project/sales.tpl.php
@@ -1,15 +1,15 @@
 <h2 class="sales_title">Mes Clients Vendeurs</h2>
-
-<?php foreach ($salesList as $currentSale) : ?>
     <div class="all_sales">
-        <div class="saleSmall">
-            <h3 class="saleSmall_client"><?= strtoupper($currentSale->getClientLastname()) . " " . ucfirst($currentSale->getClientFirstname()) ?></h3>
-            <div class="saleSmall_type"><?= ucfirst($currentSale->getTypeName()) ?></div>
-            <div class="saleSmall_rooms"><?= $currentSale->getProjectRooms() ?> chambres</div>
-            <div class="saleSmall_location"><?= ucfirst($currentSale->getProjectLocation()) ?></div>
-            <div class="saleSmall_price">Estimation : <?= $currentSale->getProjectPrice() . " €" ?></div>
-            <a href="<?= $router->generate('sale', ['idProject' => $currentSale->getId()])?>">Lien détails</a>
-        </div>
-        
-    </div>
+    <?php foreach ($salesList as $currentSale) : ?>  
+        <a href="<?= $router->generate('sale', ['idProject' => $currentSale->getId()])?>">
+            <div class="saleSmall">
+                <h3 class="saleSmall_client"><?= strtoupper($currentSale->getClientLastname()) . " " . ucfirst($currentSale->getClientFirstname()) ?></h3>
+                <div class="saleSmall_type"><?= ucfirst($currentSale->getTypeName()) ?></div>
+                <div class="saleSmall_rooms"><?= $currentSale->getProjectRooms() ?> chambres</div>
+                <div class="saleSmall_location"><?= ucfirst($currentSale->getProjectLocation()) ?></div>
+                <div class="saleSmall_price">Estimation : <?= $currentSale->getProjectPrice() . " €" ?></div>
+            </div>
+        </a>
     <?php endforeach ?>
+
+    </div>
